0e2d1aaa1381d3d2b950aacc187ec0f6a44d969d
miyama22 <pdi.nds012@gmail.com>
2025-02-20 13:37:34 +0900

    行数・クエリを表示

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from google.cloud import bigquery
 import pandas as pd
-import polars as pl
-import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title='TOPページ',
@@ -12,25 +10,31 @@ st.set_page_config(
 
 st.title('シカゴ タクシービッグデータ分析')
 
+# クエリの定義
+QUERY="""
+    select
+        count(*) as cnt,
+        min(trip_start_timestamp) as min_time,
+        max(trip_start_timestamp) as max_time
+    from bigquery-public-data.chicago_taxi_trips.taxi_trips
+"""
+
 def run_query():
     # クライアントの初期化
     client = bigquery.Client()
     
-    # クエリの定義
-    query="""
-        select
-            min(trip_start_timestamp) as min_time,
-            max(trip_start_timestamp) as max_time
-        from bigquery-public-data.chicago_taxi_trips.taxi_trips
-    """
     try:
         # クエリの実行
-        query_job = client.query(query)
+        query_job = client.query(QUERY)
         return query_job.result().to_dataframe()
     except Exception as e:
-        print(f'クエリ実行中にエラーが発声しました:{e}')
+        print(f'クエリ実行中にエラーが発生しました:{e}')
         return None
 
-if st.button('クエリ実行'):
+if st.button('データの概要を確認'):
+    st.code(QUERY, language='sql')
     df = run_query()
-    st.write(df)
+    if df is None:
+        st.write('クエリ実行中にエラーが発生しました')
+    else:
+        st.write(df)
